a68126408f87099ceed2ade7ee94d2a35d6feb62
Sonny <sonnyjustine06@gmail.com>
2021-08-15 14:59:36 +0800

    Updates

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    /*var counter = 0;
+    var lastPointScroll = 0;
+    var test1 = -3000;
+    $('#test').scroll(function (){
+        var a = $(this).scrollTop();
+        if(a == 0) {
+            counter = 0;
+            test1 = -3000;
+        } else if (a > lastPointScroll) {
+            counter += 0.05;
+        } else if (counter - 0.05 > 0) {
+            counter -= 0.05;
+        }
+
+        console.log(counter);
+        $('#test1').css('transform','translateZ(' + (test1 + (counter*500)) + 'px)');
+        lastPointScroll = a;
+    });*/
+})
